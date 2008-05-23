c831380ac0564e34da4b13e6154e405c12ea8209
lilo_booter <Unknown>
2008-05-23 09:55:20 +0000

    Corrections

--- openbuild/sysbuild.py
+++ openbuild/sysbuild.py
@@ -15,42 +15,42 @@ def build( self, path, deps = [] ):
 	return result
 
 def shared_library( self, lib, sources, headers=None, pre=None, nopre=None, **keywords ):
-    """ Build a shared library ( dll or so )
-        
-        Keyword arguments:
-        lib -- The name of the library to build
-        sources -- A list of c++ source files
-        headers -- A list of c++ header files, not needed for the build, but could be used 
-                    by certain build_managers that create IDE files.
-        pre -- A tuple with two elements, the first is the name of the header file that is 
-                included in all source-files using precompiled headers, the second is the
-                name of the actual source file creating the precompiled header file itself.
-        nopre -- If pre is set, this parameter contains a list of source files (that should
-                    be present in sources) that will not use precompiled headers. These files
-                    do not include the header file in pre[0].
-        keywords -- All other parameters passed with keyword assignment.
-    """
-        
+	"""Build a shared library ( dll or so )
+		
+		Keyword arguments:
+		lib -- The name of the library to build
+		sources -- A list of c++ source files
+		headers -- A list of c++ header files, not needed for the build, but could be used 
+					by certain build_managers that create IDE files.
+		pre -- A tuple with two elements, the first is the name of the header file that is 
+				included in all source-files using precompiled headers, the second is the
+				name of the actual source file creating the precompiled header file itself.
+		nopre -- If pre is set, this parameter contains a list of source files (that should
+					be present in sources) that will not use precompiled headers. These files
+					do not include the header file in pre[0].
+		keywords -- All other parameters passed with keyword assignment.
+	"""
+		
 	if self[ 'PLATFORM' ] == 'darwin':
 		self.Append( LINKFLAGS = [ '-Wl,-install_name', '-Wl,%s/lib%s.dylib' % ( self[ 'install_name' ], lib ) ] )
-	return self.SharedLibrary( lib, sources, *kw )
+	return self.SharedLibrary( lib, sources, **keywords )
 
 def plugin( self, lib, sources, headers=None, pre=None, nopre=None, **keywords ):
-	""" Build a plugin
-    
-        Keyword arguments:
-        lib -- The name of the library to build
-        sources -- A list of c++ source files
-        headers -- A list of c++ header files, not needed for the build, but could be used 
-                    by certain build_managers that create IDE files.
-        pre -- A tuple with two elements, the first is the name of the header file that is 
-                included in all source-files using precompiled headers, the second is the
-                name of the actual source file creating the precompiled header file itself.
-        nopre -- If pre is set, this parameter contains a list of source files (that should
-                    be present in sources) that will not use precompiled headers. These files
-                    do not include the header file in pre[0].
-        keywords -- All other parameters passed with keyword assignment."""
+	"""Build a plugin
+	
+		Keyword arguments:
+		lib -- The name of the library to build
+		sources -- A list of c++ source files
+		headers -- A list of c++ header files, not needed for the build, but could be used 
+					by certain build_managers that create IDE files.
+		pre -- A tuple with two elements, the first is the name of the header file that is 
+				included in all source-files using precompiled headers, the second is the
+				name of the actual source file creating the precompiled header file itself.
+		nopre -- If pre is set, this parameter contains a list of source files (that should
+					be present in sources) that will not use precompiled headers. These files
+					do not include the header file in pre[0].
+		keywords -- All other parameters passed with keyword assignment."""
 
-	return self.SharedLibrary( lib, sources, *kw )
+	return self.SharedLibrary( lib, sources, **keywords )
 
 
