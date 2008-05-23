664ec20348f8c8c6cfb879fab763ef0f4f8e9e5e
gransell <Unknown>
2008-05-23 10:28:38 +0000

    Changed build flags

--- build/ardome-ml/osx_build_step.sh
+++ build/ardome-ml/osx_build_step.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-echo "Building Release version..."
-scons -j 2 debug=0 || exit 1
-
-echo "Building Debug version..."
+echo "Building Release and Debug versions..."
 scons -j 2 || exit 1
 
