8374481e79ad41ae80869bdef96cb9c0121926e1
lilo_booter <Unknown>
2008-06-14 11:14:24 +0000

    Better error checking

--- build/ardome-ml/ubuntu32_build_zip.sh
+++ build/ardome-ml/ubuntu32_build_zip.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-mkdir -p aml/debug
-mv build/debug/ubuntu32/bin build/debug/ubuntu32/include build/debug/ubuntu32/lib build/debug/ubuntu32/share aml/debug
+echo "Moving content from debug"
+mkdir -p aml/debug || exit 1
+mv build/debug/ubuntu32/bin build/debug/ubuntu32/include build/debug/ubuntu32/lib build/debug/ubuntu32/share aml/debug || exit 1
 
-mkdir -p aml/release
-mv build/release/ubuntu32/bin build/release/ubuntu32/include build/release/ubuntu32/lib build/release/ubuntu32/share aml/release
+echo "Moving content from release"
+mkdir -p aml/release || exit 1
+mv build/release/ubuntu32/bin build/release/ubuntu32/include build/release/ubuntu32/lib build/release/ubuntu32/share aml/release || exit 1
 
 echo "Compressing result ()"
 tar -cjf $TARGET aml
--- build/ardome-ml/ubuntu64_build_zip.sh
+++ build/ardome-ml/ubuntu64_build_zip.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-mkdir -p aml/debug
-mv build/debug/ubuntu64/bin build/debug/ubuntu64/include build/debug/ubuntu64/lib64 build/debug/ubuntu64/share aml/debug
+echo "Moving content from debug"
+mkdir -p aml/debug || exit 1
+mv build/debug/ubuntu64/bin build/debug/ubuntu64/include build/debug/ubuntu64/lib64 build/debug/ubuntu64/share aml/debug || exit 1
 
-mkdir -p aml/release
-mv build/release/ubuntu64/bin build/release/ubuntu64/include build/release/ubuntu64/lib64 build/release/ubuntu64/share aml/release
+echo "Moving content from release"
+mkdir -p aml/release || exit 1
+mv build/release/ubuntu64/bin build/release/ubuntu64/include build/release/ubuntu64/lib64 build/release/ubuntu64/share aml/release || exit 1
 
-echo "Compressing result ()"
+echo "Compressing result"
 tar -cjf $TARGET aml
 if [ ! $? == 0 ]; then
 	echo Failed to compress
