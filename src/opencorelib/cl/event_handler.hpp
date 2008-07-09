ed197885faa75f83b0c0db0cad1c15fdbdef9c14
mats_lindelof <Unknown>
2008-07-09 06:47:10 +0000

    Adding possibility to remove thread-safety from the event_handler class.

--- src/opencorelib/cl/event_handler.hpp
+++ src/opencorelib/cl/event_handler.hpp
@@ -30,13 +30,47 @@ namespace olib
         typedef boost::shared_ptr< event_connection > event_connection_ptr;
         typedef boost::weak_ptr< event_connection > weak_event_connection_ptr;
 
+        /// Provides thread-safe event_handler instances.
+        class thread_locker
+        {
+        public:
+            class lock
+            {
+            public:
+                lock( const thread_locker& lck ) : m_lck( lck.m_mtx ){}
+            private:
+                boost::recursive_mutex::scoped_lock m_lck;
+            };
+        private:
+             mutable boost::recursive_mutex m_mtx;
+        };
+
+        /// Use this class to get non-thread-safe event_handlers, pass it in as the ThreadLocker argument.
+        /** If you know that your event_handle doesn't need thread-safety, 
+            use this class as the template argument to ThreadLocker to get a speed increase */
+        class not_thread_safe
+        {
+        public:
+            class lock
+            {
+            public:
+                lock( const not_thread_safe& lck ) { }
+            private:
+            };
+        };
+
         /// Thread safe alternative to boost::signals.
         /** Does not require any library files, just a simple header.
             Less generic than boost::signals since it always takes two
             parameters. If you want to pass more parameters, use the 
             second template argument and pass a class-instance there instead. 
+
+            The third template parameter allows you to control if a mutex is going 
+            to be locked every time someone access the event_handler. The default
+            is that it will, and should normally be used. 
+
             @author Mats Lindel&ouml;f*/
-        template < class Sender, class EventArgs >
+        template < class Sender, class EventArgs, class ThreadLocker = thread_locker >
         class event_handler
         {
         public:
@@ -54,7 +88,7 @@ namespace olib
             template< class T >
             void connect( const boost::shared_ptr<T>& registrant, void (T::*CbFunc)(const Sender&, const EventArgs& )  )
             {
-                boost::recursive_mutex::scoped_lock lck(m_mtx);
+                ThreadLocker::lock lck( m_locker );
                 // Don't save a shared_ptr in the function-object, since that will 
                 // destroy the automatic connection management. We store a weak-ptr to 
                 // the object and make sure that it is alive before we ever call on the bound object.
@@ -66,7 +100,7 @@ namespace olib
             /** When the returned object goes out of scope, the connection goes down automatically. */
             event_connection_ptr connect( callback_signature cb  )
             {
-                boost::recursive_mutex::scoped_lock lck(m_mtx);
+                ThreadLocker::lock lck( m_locker );
                 event_connection_ptr conn( new event_connection() );
                 m_callbacks.push_back( std::make_pair(conn, cb) );
                 return conn;
@@ -82,7 +116,7 @@ namespace olib
                 callback_collection copy_of_callbacks;
 
                 {
-                    boost::recursive_mutex::scoped_lock lck(m_mtx);
+                    ThreadLocker::lock lck( m_locker );
                     // Clean up dead references:
                     typename callback_collection::iterator it( m_callbacks.begin()), eit(m_callbacks.end());
                     for( ; it != eit; ++it )
@@ -116,7 +150,8 @@ namespace olib
             typedef std::pair< weak_object_ptr, callback_signature > registrant_callback_pair;
             typedef std::list< registrant_callback_pair > callback_collection;
             mutable callback_collection m_callbacks;
-            mutable boost::recursive_mutex m_mtx;
+            ThreadLocker m_locker;
+            // mutable boost::recursive_mutex m_mtx;
 
         };
 
