cb227af900c89342b680d3618b6691447fda7f60
gransell <Unknown>
2008-07-17 11:14:56 +0000

    Added LSB 3.1 target

---
+++ build/ardome-ml/lsb_3_1_64_postgrab.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+cd bcomp
+
+echo "Extracting loki..."
+tar xjf loki.tbz2
+if [ ! $? == 0 ]; then
+  echo "Failed to unpack loki. Terminating."
+  exit 1
+fi
+
+echo "Extracting boost..."
+tar xjf boost.tbz2
+if [ ! $? == 0 ]; then
+  echo "Failed to unpack boost. Terminating."
+  exit 1
+fi
+
+echo "Extracting ffmpeg..."
+tar xjf ffmpeg.tbz2
+if [ ! $? == 0 ]; then
+  echo "Failed to unpack ffmpeg. Terminating."
+  exit 1
+fi
+
+echo "Extracting xercesc..."
+tar xjf xercesc.tbz2
+if [ ! $? == 0 ]; then
+  echo "Failed to unpack xercesc. Terminating."
+  exit 1
+fi
