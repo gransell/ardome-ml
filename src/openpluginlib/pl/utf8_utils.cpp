52bb905ee48099f06739ad67421be00a5db268d1
mats_lindelof <Unknown>
2008-08-22 12:24:09 +0000

    Using correct string conversions from opencorelib (utf8 <-> utf16)

--- src/openpluginlib/pl/utf8_utils.cpp
+++ src/openpluginlib/pl/utf8_utils.cpp
@@ -9,47 +9,20 @@
 #include <vector>
 
 #include <openpluginlib/pl/utf8_utils.hpp>
+#include "opencorelib/cl/core.hpp"
+#include "opencorelib/cl/str_util.hpp"
+
 
 namespace olib { namespace openpluginlib {
 
 wstring to_wstring( const string& str )
 {
-	std::vector<wchar_t> ws;
-				
-	ws.resize( str.size( ), 0 );
-
-#if _MSC_VER >= 1400
-	ws.push_back( 0 ); // microsoft secure code initiative or whatever...
-
-	size_t size;
-	mbstowcs_s( &size, &ws[ 0 ], ws.size( ), str.c_str( ), str.size( ) );
-	
-	return wstring( &ws[ 0 ] );
-#else
-	mbstowcs( &ws[ 0 ], str.c_str( ), str.size( ) );
-	
-	return wstring( ws.begin( ), ws.end( ) );
-#endif
+    return opencorelib::str_util::to_wstring( str );
 }
 			
 string to_string( const wstring& str )
 {
-	std::vector<char> mbs;
-			
-	mbs.resize( str.size( ) );
-
-#if _MSC_VER >= 1400
-	mbs.push_back( 0 ); // microsoft secure code initiative or whatever...
-
-	size_t size;
-	wcstombs_s( &size, &mbs[ 0 ], mbs.size( ), str.c_str( ), str.size( ) );
-	
-	return string( &mbs[ 0 ] );
-#else
-	wcstombs( &mbs[ 0 ], str.c_str( ), str.size( ) );
-	
-	return string( mbs.begin( ), mbs.end( ) );
-#endif
+    return opencorelib::str_util::to_string(str);
 }
 
 } }
