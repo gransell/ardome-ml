2f94ebc792d443cfe34648fd868dc676c8b540d7
mats_lindelof <Unknown>
2008-11-03 11:05:17 +0000

    Making openbuild a separate target.

---
+++ build/openbuild/build_tar.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+echo "Trying to build openbuild.tar.bz2 target, creating directory named TMPZIP..."
+rm -rf TMPZIP
+
+mkdir TMPZIP || exit 1
+
+echo "Adding cross-platform script files..."
+cp -r openbuild TMPZIP/
+
+if [ ! $? == 0 ]; then
+	echo "Failed to copy script files files"
+	exit 1
+fi
+echo "Purging .svn folders"
+find TMPZIP -name \.svn -type d -prune -print0 | xargs -0 rm -rf
+
+echo "Purging .pyc files"
+find TMPZIP -name \*.pyc -type f -prune -print0 | xargs -0 rm -rf
+
+echo "Building tar-file openbuild.tar.bz2 ..."
+cd TMPZIP 
+tar -jcf openbuild.tar.bz2 *
+
+if [ ! $? == 0 ]; then
+	echo "Failed to execture: tar -jcf openbuild.tar.bz2 *"
+	exit 1
+fi
+	
+cd ..	
+echo "Copying openbuild.tar.bz2 to folder where prod was issued..."
+mv TMPZIP/openbuild.tar.bz2 ./ 
+
+echo "Removing temporary folder, almost done..."
+rm -rf TMPZIP
+
+echo "Product openbuild.tar.bz2 created successfully!"
