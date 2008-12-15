46f889ec759e065bc67d98f955a8394ee1e7c4c0
Mats Lindelof <mats.lindelof@vizrt.com>
2008-12-15 16:16:17 +0100

    Added default constructor to point

--- src/opencorelib/cl/point.hpp
+++ src/opencorelib/cl/point.hpp
@@ -14,6 +14,9 @@ namespace olib
                                 public boost::subtractable< point >
         {
         public:
+			
+			point() : m_x(0.0), m_y(0.0) {}
+			
             point( double x, double y) 
                 : m_x(x), m_y(y) {}
 
