cea774651e50750a2927293f97574ea9eefee435
Mikael Gransell <mikael.gransell@vizrt.com>
2009-09-30 22:05:03 +0200

    Added pf report on stream in null store

--- src/openmedialib/plugins/ardendo/utility.cpp
+++ src/openmedialib/plugins/ardendo/utility.cpp
@@ -459,11 +459,11 @@ void report_frame( std::ostream &stream, const ml::frame_type_ptr &frame )
 	stream << "Frame Report" << endl << endl;
 	stream << "Position    : " << frame->get_position( ) << endl;
 	if ( frame->has_image( ) )
-		stream << "Image Info  : pf: " << pl::to_string( frame->pf( ) ) << ", codec = " << frame->video_codec( ) << ", size = " << frame->width( ) << "x" << frame->height( ) << ", sar = " << frame->get_sar_num( ) << ":" << frame->get_sar_den( ) << std::endl;
+		stream << "Image Info  : pf = " << pl::to_string( frame->pf( ) ) << ", codec = " << frame->video_codec( ) << ", size = " << frame->width( ) << "x" << frame->height( ) << ", sar = " << frame->get_sar_num( ) << ":" << frame->get_sar_den( ) << std::endl;
 	if ( frame->has_audio( ) )
 		stream << "Audio Info  : codec = " << frame->audio_codec( ) << ", samples = " << frame->samples( ) << ", frequency = "  << frame->frequency( ) << ", channels = " << frame->channels( ) << std::endl;
 	if ( frame->get_stream( ) )
-		stream << "Has Stream  : Yes, key = " << frame->get_stream( )->key( ) << ", position = " << frame->get_stream( )->position( ) << ", bitrate = " << frame->get_stream( )->bitrate( ) << endl;
+		stream << "Has Stream  : Yes, pf = " << pl::to_string( frame->get_stream( )->pf( ) ) << " key = " << frame->get_stream( )->key( ) << ", position = " << frame->get_stream( )->position( ) << ", bitrate = " << frame->get_stream( )->bitrate( ) << endl;
 	else
 		stream << "Has Stream  : No" << endl;
 
