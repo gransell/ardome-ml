93a09cacf2c888c7c0a52be0dfc5243fcc53a39e
Macdev <macdev@gullbritt.ardendo.se>
2010-04-11 18:58:33 +0200

    MINOR: Copy resources to result for osx

--- build/ardome-ml/osx_build_zip.sh
+++ build/ardome-ml/osx_build_zip.sh
@@ -2,10 +2,10 @@
 
 echo "Copying libs and headers to temporary dir..."
 mkdir -p aml/debug
-mv build/debug/osx/bin build/debug/osx/include build/debug/osx/lib build/debug/osx/share aml/debug
+mv build/debug/osx/bin build/debug/osx/include build/debug/osx/lib build/debug/osx/Resourcesi build/debug/osx/share aml/debug
 
 mkdir -p aml/release
-mv build/release/osx/bin build/release/osx/include build/release/osx/lib build/release/osx/share aml/release
+mv build/release/osx/bin build/release/osx/include build/release/osx/lib build/release/osx/Resources build/release/osx/share aml/release
 
 echo Compressing result...
 tar -cjf ardome-ml-osx-10.5.tbz2 aml/ > /dev/null
