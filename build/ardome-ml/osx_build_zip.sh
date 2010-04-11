7043bac60120d2987466506009a3b1bb48881cff
Macdev <macdev@gullbritt.ardendo.se>
2010-04-11 19:05:26 +0200

    MINOR: Typo

--- build/ardome-ml/osx_build_zip.sh
+++ build/ardome-ml/osx_build_zip.sh
@@ -2,7 +2,7 @@
 
 echo "Copying libs and headers to temporary dir..."
 mkdir -p aml/debug
-mv build/debug/osx/bin build/debug/osx/include build/debug/osx/lib build/debug/osx/Resourcesi build/debug/osx/share aml/debug
+mv build/debug/osx/bin build/debug/osx/include build/debug/osx/lib build/debug/osx/Resources build/debug/osx/share aml/debug
 
 mkdir -p aml/release
 mv build/release/osx/bin build/release/osx/include build/release/osx/lib build/release/osx/Resources build/release/osx/share aml/release
