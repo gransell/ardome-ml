b0a908c630863c4ee50bc2b041d4054c26b73477
Mattias Eriksson <mattias.eriksson@vizrt.com>
2010-09-29 09:53:51 +0200

    Set the AMF_PREFIX_PATH should get the tests to work

--- build/ardome-ml/lsb_3_1_32_test_step.sh
+++ build/ardome-ml/lsb_3_1_32_test_step.sh
@@ -2,6 +2,7 @@
 
 export LD_LIBRARY_PATH=`pwd`/build/release/lsb_3_1_32/lib/
 export AML_PATH=`pwd`/build/release/lsb_3_1_32/lib/ardome-ml/openmedialib/plugins/
+export AMF_PREFIX_PATH=`pwd`/build/release/lsb_3_1_32/
 
 build/release/lsb_3_1_32/bin/opencore_unit_tests all_tests
 
