3577d423929fe59bcfb97d56dc89f8a8fc9b454e
unknown <gransell@ardendo.se>
2011-01-31 21:24:16 +0100

    Added support for settings thread name on windows. Compiles at least...

--- src/opencorelib/cl/thread_name.cpp
+++ src/opencorelib/cl/thread_name.cpp
@@ -1,24 +1,29 @@
 
 #include "precompiled_headers.hpp"
 #include "thread_name.hpp"
+#include "str_util.hpp"
+
+#if defined( OLIB_ON_MAC )		
+#include <pthread.h>
+#include <AvailabilityMacros.h>
+
+#elif defined( OLIB_ON_LINUX )
+#include <sys/prctl.h>
+#endif
 
 namespace olib { namespace opencorelib {
 	
 	namespace {
 		#if defined( OLIB_ON_MAC )
-		
-		#include <pthread.h>
-        #include <AvailabilityMacros.h>
-		void set_thread_name_internal( const olib::t_string& name )
+		void set_thread_name_internal( const char *name )
 		{
             // This function is only available on 10.6 and above
 #if MAC_OS_X_VERSION_MIN_REQUIRED >= MAC_OS_X_VERSION_10_6
-			pthread_setname_np( name.c_str( ) );
+			pthread_setname_np( name );
 #endif
 		}
 		
 		#elif defined( OLIB_ON_WINDOWS )
-
 		typedef struct tagTHREADNAME_INFO
 		{
 		   DWORD dwType; // must be 0x1000
@@ -27,13 +32,12 @@ namespace olib { namespace opencorelib {
 		   DWORD dwFlags; // reserved for future use, must be zero
 		} THREADNAME_INFO;
 
-		void set_thread_name_internal( const olib::t_string& name )
+		void set_thread_name_internal( const char *name )
 		{
-			std::string name_utf8 = str_util::to_string( name );
 			THREADNAME_INFO info;
 			info.dwType = 0x1000;
-			info.szName = name_utf8.c_str( );
-			info.dwThreadID = get_current_thread_id( );;
+			info.szName = name;
+			info.dwThreadID = -1;
 			info.dwFlags = 0;
 
 			__try
@@ -44,19 +48,19 @@ namespace olib { namespace opencorelib {
 			{
 			}
 		}
+
 		#elif defined( OLIB_ON_LINUX )
-		
-		#include <sys/prctl.h>
-		void set_thread_name_internal( const olib::t_string& name )
+		void set_thread_name_internal( const char *name )
 		{
-			prctl( PR_SET_NAME, name.c_str( ), 0, 0, 0 );
+			prctl( PR_SET_NAME, name, 0, 0, 0 );
 		}
 		#endif
 	}
 	
 	CORE_API void set_thread_name( const olib::t_string& name )
 	{
-		set_thread_name_internal( name );
+		std::string utf8_name = str_util::to_string( name );
+		set_thread_name_internal( utf8_name.c_str( ) );
 	}
 	
 } /* opencorelib */
