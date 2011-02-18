f6d8f3b4436a361e9329728e2d9ab01bcb81ae7c
Mikael Gransell <mikael.gransell@vizrt.com>
2011-02-18 16:22:55 +0100

    Changed update-rc.sh to call prebag-rc instead of pretag-rc

--- build/ardome-ml/update-rc.sh
+++ build/ardome-ml/update-rc.sh
@@ -4,20 +4,20 @@ if [ "$BUILDNUMBER" != "LATEST" ]; then
 
 echo "Updating RC files..."
 
-pretag-rc src/opencorelib/cl/msw/core.rc
-pretag-rc src/openimagelib/il/msw/imagelib.rc
-pretag-rc src/openmedialib/ml/msw/medialib.rc
-pretag-rc src/openmedialib/plugins/ardendo/aml_filters.rc
-pretag-rc src/openmedialib/plugins/avformat/msw/avformatplugin.rc
-pretag-rc src/openmedialib/plugins/decode/msw/decodeplugin.rc
-pretag-rc src/openmedialib/plugins/gensys/msw/gensysplugin.rc
-pretag-rc src/openmedialib/plugins/mlt/msw/mltplugin.rc
-pretag-rc src/openmedialib/plugins/rubberband/msw/rubberbandplugin.rc
-pretag-rc src/openmedialib/plugins/sdl/msw/sdlplugin.rc
-pretag-rc src/openmedialib/plugins/sox/msw/soxplugin.rc
-pretag-rc src/openmedialib/plugins/wav/msw/wavplugin.rc
-pretag-rc src/openmedialib/tools/amlbatch/amlbatch.rc
-pretag-rc src/openpluginlib/pl/msw/pluginlib.rc
+prebag-rc src/opencorelib/cl/msw/core.rc
+prebag-rc src/openimagelib/il/msw/imagelib.rc
+prebag-rc src/openmedialib/ml/msw/medialib.rc
+prebag-rc src/openmedialib/plugins/ardendo/aml_filters.rc
+prebag-rc src/openmedialib/plugins/avformat/msw/avformatplugin.rc
+prebag-rc src/openmedialib/plugins/decode/msw/decodeplugin.rc
+prebag-rc src/openmedialib/plugins/gensys/msw/gensysplugin.rc
+prebag-rc src/openmedialib/plugins/mlt/msw/mltplugin.rc
+prebag-rc src/openmedialib/plugins/rubberband/msw/rubberbandplugin.rc
+prebag-rc src/openmedialib/plugins/sdl/msw/sdlplugin.rc
+prebag-rc src/openmedialib/plugins/sox/msw/soxplugin.rc
+prebag-rc src/openmedialib/plugins/wav/msw/wavplugin.rc
+prebag-rc src/openmedialib/tools/amlbatch/amlbatch.rc
+prebag-rc src/openpluginlib/pl/msw/pluginlib.rc
 
 
 echo "Updating RC files done."
