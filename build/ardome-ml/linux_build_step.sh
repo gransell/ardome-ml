db27639f422a93834153b2c00bc678d29c4a355a
Gustaf Räntilä <gra@vizrt.com>
2011-04-14 15:49:34 +0200

    Made building on Linux use all cores

--- build/ardome-ml/linux_build_step.sh
+++ build/ardome-ml/linux_build_step.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 echo "Starting Unix toolchain build of ardome-ml"
-scons target=$BAGTARGET -j4 || exit 1
+scons target=$BAGTARGET -j`cat /proc/cpuinfo | grep processor | wc -l` || exit 1
