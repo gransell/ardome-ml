8ed2a5e56486b227c830b5b885785c50a8ea7706
Charlie <cya@vizrt.com>
2012-03-29 16:57:28 +0200

    Support for ubuntu64 and boost 1.4x, removal of unecessary libxml2 dependency

--- build/ardome-ml/ubuntu_check_deps.sh
+++ build/ardome-ml/ubuntu_check_deps.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 missing=''
-for pkg in 'libloki-dev' 'libxml2-dev' 'libxerces-c2-dev' 'uuid-dev' 'libsdl1.2-dev' 'libboost-dev' 'libboost-date-time-dev' 'libboost-filesystem-dev' 'libboost-python-dev' 'libboost-regex-dev' 'libboost-signals-dev' 'libboost-test-dev' 'libboost-thread-dev'
+for pkg in 'libloki-dev' 'libxerces-c2-dev' 'uuid-dev' 'libsdl1.2-dev' 'libboost-dev' 'libboost-date-time-dev' 'libboost-filesystem-dev' 'libboost-python-dev' 'libboost-regex-dev' 'libboost-signals-dev' 'libboost-test-dev' 'libboost-thread-dev'
 do
 	if dpkg-query --status $pkg > /dev/null 2>&1; then
     	: ok
