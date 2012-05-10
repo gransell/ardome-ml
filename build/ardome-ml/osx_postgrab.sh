4fd8a6c5537ad119416ca63202ff61db3cd8cb4a
Mikael Gransell <mikael.gransell@vizrt.com>
2012-05-10 06:58:36 +0200

    MINOR: Make sure the postgrab script does not fail if one or more files are not there

--- build/ardome-ml/osx_postgrab.sh
+++ build/ardome-ml/osx_postgrab.sh
@@ -31,24 +31,45 @@ while read dylib ;
 done
 )
 
-echo Extracting Xercesc...
-tar -jxf xercesc.tar.bz2 || exit 1
+if [ -e "xercesc.tar.bz2" ] 
+	then
+	echo Extracting Xercesc...
+	tar -jxf xercesc.tar.bz2 || exit 1
+fi
 
-echo "Extracting boost..."
-tar -jxf boost.tar.bz2 || exit 1
+if [ -e "boost.tar.bz2" ] 
+	then
+	echo "Extracting boost..."
+	tar -jxf boost.tar.bz2 || exit 1
+fi
 
-echo Extracting ffmpeg...
-tar -jxf ffmpeg.tar.bz2 || exit 1
+if [ -e "ffmpeg.tar.bz2" ] 
+	then
+	echo Extracting ffmpeg...
+	tar -jxf ffmpeg.tar.bz2 || exit 1
+fi
 
-echo Extracting loki...
-tar -xjf loki.tbz2 || exit 1
+if [ -e "loki.tbz2" ] 
+	then
+	echo Extracting loki...
+	tar -xjf loki.tbz2 || exit 1
+fi
 
-echo 'Extracting doxygen'
-tar xjf doxygen.tbz2 || exit 1
+if [ -e "doxygen.tbz2" ] 
+	then
+	echo 'Extracting doxygen'
+	tar xjf doxygen.tbz2 || exit 1
+fi
 
-echo 'Extracing sox'
-tar xjf sox.tbz2 || exit 1
+if [ -e "sox.tbz2" ] 
+	then
+	echo 'Extracing sox'
+	tar xjf sox.tbz2 || exit 1
+fi
 
-echo 'Extracting Rubberband'
-tar xjf rubberband.tbz2 || exit 1
+if [ -e "rubberband.tbz2" ] 
+	then
+	echo 'Extracting Rubberband'
+	tar xjf rubberband.tbz2 || exit 1
+fi
 
