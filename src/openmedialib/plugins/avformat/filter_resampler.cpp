5abfbfd51695821988934d86f1aeea985e2a1602
charlie <charles.yates@gmail.com>
2012-05-15 14:01:14 +0200

    Handles reverse audio in the ffmpeg resampler to avoid clicks in the output

--- src/openmedialib/plugins/avformat/filter_resampler.cpp
+++ src/openmedialib/plugins/avformat/filter_resampler.cpp
@@ -27,6 +27,8 @@ namespace pcos = olib::openpluginlib::pcos;
 
 namespace olib { namespace openmedialib { namespace ml { 
 
+static pl::pcos::key key_audio_reversed_ =  pl::pcos::key::from_string( "audio_reversed" );
+
 class ML_PLUGIN_DECLSPEC avformat_resampler_filter : public filter_simple
 {
 	public:
@@ -44,6 +46,8 @@ class ML_PLUGIN_DECLSPEC avformat_resampler_filter : public filter_simple
 			, dirty_(true)
 			, in_buffer_( )
 			, out_buffer_( )
+			, direction_( 1 )
+			, expected_( -1 )
 		{
 			property_observer_ = boost::shared_ptr<pcos::observer>(new avformat_resampler_filter::property_observer(const_cast<avformat_resampler_filter*>(this)));
 		
@@ -83,13 +87,24 @@ class ML_PLUGIN_DECLSPEC avformat_resampler_filter : public filter_simple
 		{
 			// Cache a local copy of current position
 			int current_pos = get_position();
-		
+
+			// Make sure we know the direction of the samples so we can correct as required
+			if ( current_pos != expected_ )
+				direction_ = expected_ - current_pos == 2 ? -1 : 1;
+
+			// Next expected frame
+			expected_ = current_pos + direction_;
+
 			// Ensure the cache is full
 			current_frame = fetch_from_slot( 0 );
 
 			if ( prop_enable_.value< int >( ) == 0 || !current_frame )
 				return;
-		
+
+			// Ensure the audio samples are in the right direction
+			current_frame = current_frame->shallow( );
+			reverse( current_frame );
+
 			ml::audio_type_ptr current_audio = audio::coerce( audio::FORMAT_PCM16, current_frame->get_audio( ) );
 			if(!current_audio)
 				return;
@@ -220,7 +235,31 @@ class ML_PLUGIN_DECLSPEC avformat_resampler_filter : public filter_simple
 			private:
 				avformat_resampler_filter* filter_;
 		};
-	
+
+		// Handles the audio direction
+		void reverse( ml::frame_type_ptr &frame )
+		{
+			ml::audio_type_ptr audio = frame->get_audio( );
+
+			if ( audio )
+			{
+				// Make sure we have a propery on the frame
+				if ( !frame->property_with_key( key_audio_reversed_ ).valid( ) )
+					frame->properties( ).append( pl::pcos::property( key_audio_reversed_ ) = 0 );
+
+				// Obtain the reverse audio property
+				pl::pcos::property property = frame->property_with_key( key_audio_reversed_ );
+
+				if ( ( direction_ < 0 && property.value< int >( ) == 0 ) || ( direction_ > 0 && property.value< int >( ) == 1 ) )
+				{
+					audio = ml::audio::reverse( audio );
+					property = ( int )( property.value< int >( ) ? 0 : 1 );
+				}
+			}
+
+			frame->set_audio( audio );
+		}
+
 		boost::shared_ptr<pcos::observer> property_observer_;
 		
 		pcos::property	prop_output_channels_;
@@ -238,6 +277,9 @@ class ML_PLUGIN_DECLSPEC avformat_resampler_filter : public filter_simple
 		std::vector< short > in_buffer_;
 		std::vector< short > out_buffer_;
 		audio_type_ptr last_channels_;
+
+		int direction_;
+		int expected_;
 };
 
 filter_type_ptr ML_PLUGIN_DECLSPEC create_resampler( const pl::wstring &resource )
