25280e8594df26581ec08abe5ef75fb185b856f0
Charles Yates <cya@vizrt.com>
2012-11-05 18:45:19 +0100

    Adds the fork filter and uses serialise_as

--- src/openmedialib/plugins/distributor/plugin_distributor.cpp
+++ src/openmedialib/plugins/distributor/plugin_distributor.cpp
@@ -5,6 +5,7 @@
 
 #include <openmedialib/ml/openmedialib_plugin.hpp>
 #include <openmedialib/ml/stack.hpp>
+#include <openmedialib/ml/keys.hpp>
 #include <openpluginlib/pl/pcos/observer.hpp>
 #include <opencorelib/cl/thread_pool.hpp>
 #include <opencorelib/cl/function_job.hpp>
@@ -279,8 +280,8 @@ class ML_PLUGIN_DECLSPEC filter_voodoo : public filter_type
 			if ( changed_ )
 			{
 				// Wipe the old lock and muxer now
-				muxer_ = ml::filter_type_ptr( );
-				lock_ = ml::filter_type_ptr( );
+				muxer_ = ml::input_type_ptr( );
+				lock_ = ml::input_type_ptr( );
 
 				// Fetch the 3 inputs
 				ml::input_type_ptr input = fetch_slot( 0 );
@@ -292,33 +293,51 @@ class ML_PLUGIN_DECLSPEC filter_voodoo : public filter_type
 				ARENFORCE_MSG( video, "Nothing connected on the video slot" );
 				ARENFORCE_MSG( audio, "Nothing connected on the audio slot" );
 
-				// Create the lock
-				lock_ = ml::create_filter( L"lock" );
-				lock_->property( "queue" ) = prop_queue_.value< int >( );
-				lock_->connect( input );
+				// Create the lock if necessary
+				if ( input->get_uri( ) != L"lock" )
+				{
+					lock_ = ml::create_filter( L"lock" );
+					pl::pcos::assign< std::wstring >( lock_->properties( ), ml::keys::serialise_as, L"" );
+					lock_->property( "queue" ) = prop_queue_.value< int >( );
+					lock_->connect( input );
+				}
+				else
+				{
+					lock_ = input;
+				}
 
 				// Replace the nudgers with the the locked input
 				video = replace_with_lock( video, 1 );
 				audio = replace_with_lock( audio, 2 );
 
 				// Everything should be connected now, so we'll grab a frame from the video and conform audio to that frame rate
-				ml::frame_type_ptr frame = video->fetch( );
+				ml::frame_type_ptr v_frame = video->fetch( );
+				ml::frame_type_ptr a_frame = audio->fetch( );
 
 				// Ensure that the frame is accesible
-				ARENFORCE_MSG( frame, "No frame from video slot" );
+				ARENFORCE_MSG( v_frame, "No frame from video slot" );
+				ARENFORCE_MSG( a_frame, "No frame from audio slot" );
 
 				// Create the frame_rate filter for the audio
-				ml::filter_type_ptr audio_fps = ml::create_filter( L"frame_rate" );
-				audio_fps->property( "fps_num" ) = frame->get_fps_num( );
-				audio_fps->property( "fps_den" ) = frame->get_fps_den( );
-				audio_fps->connect( audio );
-				audio_fps->sync( );
+				if ( v_frame->get_fps_num( ) != a_frame->get_fps_num( ) || v_frame->get_fps_den( ) != a_frame->get_fps_den( ) )
+				{
+					ml::filter_type_ptr audio_fps = ml::create_filter( L"frame_rate" );
+					pl::pcos::assign< std::wstring >( audio_fps->properties( ), ml::keys::serialise_as, L"" );
+					audio_fps->property( "fps_num" ) = v_frame->get_fps_num( );
+					audio_fps->property( "fps_den" ) = v_frame->get_fps_den( );
+					audio_fps->connect( audio );
+					audio = audio_fps;
+				}
+
+				// Make sure that the rest of the world sees the correct graphs
+				connect( video, 1 );
+				connect( audio, 2 );
 
 				// Construct the muxer
 				muxer_ = ml::create_filter( L"muxer" );
 				muxer_->property( "use_longest" ) = 0;
 				muxer_->connect( video, 0 );
-				muxer_->connect( audio_fps, 1 );
+				muxer_->connect( audio, 1 );
 
 				// Sync the muxer
 				muxer_->sync( );
@@ -337,6 +356,7 @@ class ML_PLUGIN_DECLSPEC filter_voodoo : public filter_type
 			{
 				// TODO: Mark conform filter so that it gets reported as nudger: in an aml dump
 				ml::filter_type_ptr conform = ml::create_filter( L"conform" );
+				pl::pcos::assign< std::wstring >( conform->properties( ), ml::keys::serialise_as, L"nudger:" );
 				conform->property( "image" ) = voodoo == 1 ? 1 : 0;
 				conform->property( "audio" ) = voodoo == 1 ? 0 : 1;
 				conform->connect( lock_ );
@@ -361,8 +381,167 @@ class ML_PLUGIN_DECLSPEC filter_voodoo : public filter_type
 
 	private:
 		pl::pcos::property prop_queue_;
-		ml::filter_type_ptr lock_;
-		ml::filter_type_ptr muxer_;
+		ml::input_type_ptr lock_;
+		ml::input_type_ptr muxer_;
+		bool changed_;
+		int frames_;
+};
+
+class ML_PLUGIN_DECLSPEC filter_fork : public filter_type
+{
+	public:
+		filter_fork( )
+		: prop_slots_( pl::pcos::key::from_string( "slots" ) )
+		, prop_queue_( pl::pcos::key::from_string( "queue" ) )
+		, prop_preroll_( pl::pcos::key::from_string( "preroll" ) )
+		, changed_( false )
+		, frames_( 0 )
+		{
+			properties( ).append( prop_slots_ = 2 );
+			properties( ).append( prop_queue_ = 50 );
+			properties( ).append( prop_preroll_ = 25 );
+		}
+
+		virtual bool requires_image( ) const { return false; }
+
+		virtual const std::wstring get_uri( ) const { return std::wstring( L"fork" ); }
+
+		virtual const size_t slot_count( ) const { return prop_slots_.value< int >( ); }
+
+		void on_slot_change( input_type_ptr input, int slot = 0 ) { changed_ = true; }
+
+		int get_frames( ) const { return frames_; }
+
+		void sync( )
+		{
+			refresh_graphs( );
+
+			if ( lock_ )
+			{
+				lock_->sync( );
+				frames_ = lock_->get_frames( ); 
+				for( size_t i = 1; i < slot_count( ); i ++ )
+					fetch_slot( i )->sync( );
+			}
+			else
+			{
+				frames_ = 0;
+			}
+		}
+
+	protected:
+		// Invoked by fetch to obtain the frame requested
+		void do_fetch( frame_type_ptr &frame )
+		{
+			// Refresh the graphs if necessary
+			refresh_graphs( );
+
+			// Check that we have a muxer to communicate with
+			ARENFORCE_MSG( lock_, "Was unable to derive the lock" );
+
+			lock_->seek( get_position( ) );
+			frame = lock_->fetch( );
+
+			for( size_t i = 1; i < slot_count( ); i ++ )
+			{
+				fetch_slot( i )->seek( get_position( ) );
+				fetch_slot( i )->fetch( );
+			}
+		}
+
+		void refresh_graphs( )
+		{
+			if ( changed_ )
+			{
+				// Wipe the old lock now
+				lock_ = ml::input_type_ptr( );
+
+				// Fetch the 3 inputs
+				ml::input_type_ptr input = fetch_slot( 0 );
+
+				// Sanitise the inputs now
+				ARENFORCE_MSG( input, "Nothing connected on the input" );
+
+				// Create the lock if necessary
+				if ( input->get_uri( ) != L"lock" )
+				{
+					lock_ = ml::create_filter( L"lock" );
+					pl::pcos::assign< std::wstring >( lock_->properties( ), ml::keys::serialise_as, L"nudger:" );
+					lock_->property( "queue" ) = prop_queue_.value< int >( );
+					lock_->connect( input );
+				}
+				else
+				{
+					lock_ = input;
+				}
+
+				// Grab a frame from the lock so that we know the frame 
+				ml::frame_type_ptr frame = lock_->fetch( );
+
+				// Replace the nudgers with the the locked input
+				for( size_t i = 1; i < slot_count( ); i ++ )
+				{
+					ml::input_type_ptr slot = fetch_slot( i );
+
+					ml::input_type_ptr graph = replace_with_lock( slot );
+					graph->sync( );
+
+					ml::frame_type_ptr temp = graph->fetch( );
+					if ( temp->get_fps_num( ) != frame->get_fps_num( ) || temp->get_fps_den( ) != frame->get_fps_den( ) )
+					{
+						ml::filter_type_ptr fps = ml::create_filter( L"frame_rate" );
+						pl::pcos::assign< std::wstring >( fps->properties( ), ml::keys::serialise_as, L"" );
+						fps->property( "fps_num" ) = frame->get_fps_num( );
+						fps->property( "fps_den" ) = frame->get_fps_den( );
+						fps->connect( graph );
+						graph->sync( );
+						graph = fps;
+					}
+
+					if ( graph != slot )
+						connect( graph, i );
+				}
+
+				// Everything should be done now
+				changed_ = false;
+			}
+		}
+
+		// Walk the graph, replacing nudgers with lock conform sub graphs
+		ml::input_type_ptr replace_with_lock( ml::input_type_ptr &input )
+		{
+			ml::input_type_ptr result = input;
+
+			if ( input->get_uri( ) == L"nudger:" )
+			{
+				// TODO: Mark lock filter so that it gets reported as nudger: in an aml dump
+				result = lock_;
+			}
+			else
+			{
+				for ( size_t i = 0; i < input->slot_count( ); i ++ )
+				{
+					ml::input_type_ptr slot = input->fetch_slot( i );
+					if ( slot )
+					{
+						ml::input_type_ptr upstream = replace_with_lock( slot );
+						if ( upstream != slot )
+							input->connect( upstream, i );
+						if ( input->get_uri( ) == L"voodoo" || input->get_uri( ) == L"fork" )
+							break;
+					}
+				}
+			}
+
+			return result;
+		}
+
+	private:
+		pl::pcos::property prop_slots_;
+		pl::pcos::property prop_queue_;
+		pl::pcos::property prop_preroll_;
+		ml::lru_frame_type lru_;
+		ml::input_type_ptr lock_;
 		bool changed_;
 		int frames_;
 };
@@ -460,6 +639,7 @@ class graphs
 				 graph->get_uri( ) == L"tee" )
 			{
 				filter_type_ptr lock = create_filter( L"lock" );
+				pl::pcos::assign< std::wstring >( lock->properties( ), ml::keys::serialise_as, L"" );
 				lock->connect( graph );
 				parent->connect( lock, parent_slot );
 				parser.push( lock );
@@ -868,6 +1048,8 @@ public:
 
 	virtual filter_type_ptr filter( const std::wstring &spec )
 	{
+		if ( spec == L"fork" )
+			return filter_type_ptr( new filter_fork( ) );
 		if ( spec == L"lock" )
 			return filter_type_ptr( new filter_lock( ) );
 		if ( spec == L"distributor" )
