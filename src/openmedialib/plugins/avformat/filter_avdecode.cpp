a48709cfc73c74312fd6c4a7d2cc5ff096978a30
Charlie Yates <cya@vizrt.com>
2013-03-01 22:10:24 +0100

    Temporary fix for AES3

--- src/openmedialib/plugins/avformat/filter_avdecode.cpp
+++ src/openmedialib/plugins/avformat/filter_avdecode.cpp
@@ -846,18 +846,16 @@ private:
 
 				decoded_audio->set_position( position );
 				
-				// If we got AV_SAMPLE_FMT_S32 out it means that we actually want 24 bit since that is
-				// the largest we support. So we shift away the lest sginificant byte in the 32 bit values
-				// In the case of AES3 decoded by use we dont have to do this since we get it in 24 bit
+				// In the case of AES3 we get 24 bit want 32 - thus we bit shift here (for now)
 				if( track_context->sample_fmt == AV_SAMPLE_FMT_S32 &&
-					track_context->codec_id != static_cast< enum CodecID >( AML_AES3_CODEC_ID ) )
+					track_context->codec_id == static_cast< enum CodecID >( AML_AES3_CODEC_ID ) )
 				{
 					boost::int32_t * start = static_cast< boost::int32_t * >( decoded_audio->pointer( ) );
 					boost::int32_t * end = start + decoded_audio->size( ) / sizeof(boost::int32_t);
 					
 					for (boost::int32_t *ptr = start; ptr != end; ++ptr)
 					{
-						*(ptr) >>= 8;
+						*(ptr) <<= 8;
 					}
 				}
 				
