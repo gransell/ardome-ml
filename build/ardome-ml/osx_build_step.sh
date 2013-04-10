79535f70b08a3d85ef1b4bc872b52e1f863f2c8a
Martin Almlof <martin.almloef@vizrt.com>
2013-04-10 11:19:13 +0200

    ardome-ml osx build: 	* updated osx_build_step to set xcode_ver=4.2 to scons (so it builds on gullbritt)

--- build/ardome-ml/osx_build_step.sh
+++ build/ardome-ml/osx_build_step.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "Building Release and Debug versions..."
-scons -j 8 wrappers=yes install_name=@executable_path/../lib/ || exit 1
+scons -j 8 wrappers=yes xcode_ver=4.2 install_name=@executable_path/../lib/ || exit 1
 
