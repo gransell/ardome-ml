7fb5c419a173c93e41131488c56dd7c46756f4e6
Dieter Tschanz <drz@vizrt.com>
2013-04-16 09:39:20 +0200

    J#AMF-1973: Split out tests to run as a separate step in Hudson, rather than inline as a build step - Optional copy of the testreports from tmp-build to hudson-workspace for test-statistics. - Writing test reports as XML and adding information for test statistics when parsed by hudson.

--- build/ardome-ml/linux_test_step.sh
+++ build/ardome-ml/linux_test_step.sh
@@ -9,3 +9,8 @@ export AMF_PREFIX_PATH=`pwd`/build/release/$BAGTARGET/
 build/release/$BAGTARGET/bin/opencore_unit_tests
 build/release/$BAGTARGET/bin/openmedialib_unit_tests
 
+if [ -n "$TEST_REPORT_PATH" ]; then
+  cp tests/test_output/opencore/opencore_test_results.xml $TEST_REPORT_PATH/
+  cp tests/test_output/openmedialib/unit_test_results.xml $TEST_REPORT_PATH/
+fi
+
--- tests/openmedialib/unit_tests/src/main.cpp
+++ tests/openmedialib/unit_tests/src/main.cpp
@@ -51,14 +51,14 @@ public:
 		using namespace olib;
 		using namespace olib::opencorelib;
 
-		unit_test_log.set_format( CLF );
-		unit_test_log.set_threshold_level( log_test_units );
+		unit_test_log.set_format( XML );
+		unit_test_log.set_threshold_level( log_successful_tests );
 
 		//Create log file
-        t_path log_dir( _CT("tests/test_output/openmedialib") );
-        utilities::make_sure_path_exists( log_dir );
-        log_file_.open( ( log_dir / t_path( _CT("unit_test_results.clf") ) ).string().c_str() );
-        ARENFORCE( log_file_.good() );
+		t_path log_dir( _CT("tests/test_output/openmedialib") );
+		utilities::make_sure_path_exists( log_dir );
+		log_file_.open( ( log_dir / t_path( _CT("unit_test_results.xml") ) ).string().c_str() );
+		ARENFORCE( log_file_.good() );
 		unit_test_log.set_stream( log_file_ );
 
 		init_pl( );
