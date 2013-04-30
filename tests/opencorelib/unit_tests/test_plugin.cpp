eb8e0c4f2ecf876f819660053045df8c967aa41b
John Talling <john.talling@vizrt.com>
2013-04-30 09:50:43 +0200

    MINOR: Removed the custom exception handling in the "test_plugin" test case, since it creates a dialog box which makes the test runner hang when running under a headless SSH session. Better to just let the exception escape the test case, and the boost unit test framework will take care of logging it.

--- tests/opencorelib/unit_tests/test_plugin.cpp
+++ tests/opencorelib/unit_tests/test_plugin.cpp
@@ -16,36 +16,29 @@ namespace fs = boost::filesystem;
 
 void test_plugin()
 {
-    try 
-    {  
-        olib::t_path schema_path = special_folder::get( special_folder::amf_resources ) / _CT( "schemas/amf-plugin.xsd" );
-        olib::t_path xml_path = special_folder::get( special_folder::amf_resources ) / _CT("examples/amf_plugin_example.xml");
-        
-		schema_map schemas;
-        schemas[_CT("http://www.ardendo.com/amf/core/")] = schema_path;
+	olib::t_path schema_path = special_folder::get( special_folder::amf_resources ) / _CT( "schemas/amf-plugin.xsd" );
+	olib::t_path xml_path = special_folder::get( special_folder::amf_resources ) / _CT("examples/amf_plugin_example.xml");
+	
+	schema_map schemas;
+	schemas[_CT("http://www.ardendo.com/amf/core/")] = schema_path;
 
-        plugin_metadata_ptr md = plugin_metadata_from_xml(xml_path, schemas);
+	plugin_metadata_ptr md = plugin_metadata_from_xml(xml_path, schemas);
 
-        olib::t_path plugin_path = special_folder::get( special_folder::plugins); 
-        plugin_loader loader;
-        std::vector<t_string> plugin_names;
-        plugin_names.push_back(_CT("plugin_test_assembly.xml"));
-        loader.load_plugin_metadata( plugin_path, schema_path, plugin_names );
+	olib::t_path plugin_path = special_folder::get( special_folder::plugins); 
+	plugin_loader loader;
+	std::vector<t_string> plugin_names;
+	plugin_names.push_back(_CT("plugin_test_assembly.xml"));
+	loader.load_plugin_metadata( plugin_path, schema_path, plugin_names );
 
-        {
-            object_ptr cptr = create_plugin< object >( _CT("test_plugin"), loader );
-            BOOST_REQUIRE_MESSAGE( cptr, "Could not create test_plugin.");
-            logtarget_ptr lt = boost::dynamic_pointer_cast< logtarget >(cptr);
-            BOOST_REQUIRE_MESSAGE( lt, "amf_standard_plugin should implement the logtarget interface");
-            the_log_handler::instance().add_target(lt);
+	{
+		object_ptr cptr = create_plugin< object >( _CT("test_plugin"), loader );
+		BOOST_REQUIRE_MESSAGE( cptr, "Could not create test_plugin.");
+		logtarget_ptr lt = boost::dynamic_pointer_cast< logtarget >(cptr);
+		BOOST_REQUIRE_MESSAGE( lt, "amf_standard_plugin should implement the logtarget interface");
+		the_log_handler::instance().add_target(lt);
 
-            ARLOG_WARN("Now the plugin should be able to log this... check file in tmp-dir!");
+		ARLOG_WARN("Now the plugin should be able to log this... check file in tmp-dir!");
 
-            the_log_handler::instance().remove_target(lt);
-        }
-    }
-    catch( base_exception& e ) {
-        base_exception::show(e);
-        BOOST_FAIL("There was an exception.  Se above.");
-    }
+		the_log_handler::instance().remove_target(lt);
+	}
 }
