161e85021c215ed992e210884364ea5363755127
Charles Yates <charles.yates@gmail.com>
2013-08-29 10:28:08 +0200

    Adds a property type for a vector of unsigned 8 bit values

--- src/openpluginlib/pl/pcos/property.cpp
+++ src/openpluginlib/pl/pcos/property.cpp
@@ -357,6 +357,11 @@ template OPENPLUGINLIB_DECLSPEC ml::stream_type_ptr property::value< ml::stream_
 template OPENPLUGINLIB_DECLSPEC void property::set< ml::stream_type_ptr >( const ml::stream_type_ptr& );
 template OPENPLUGINLIB_DECLSPEC bool property::is_a< ml::stream_type_ptr >() const;
 
+typedef std::vector< boost::uint8_t > unsigned_chars;
+
+template OPENPLUGINLIB_DECLSPEC unsigned_chars property::value< unsigned_chars >() const;
+template OPENPLUGINLIB_DECLSPEC bool property::is_a< unsigned_chars >() const;
+
 
 // implementation of parsing code for pcos::any
 
