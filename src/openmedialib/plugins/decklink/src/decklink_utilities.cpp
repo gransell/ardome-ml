8d27d7c3130388775ec6bec0d8790d799b036b9d
John Talling <john.talling@vizrt.com>
2013-09-25 17:39:29 +0200

    MINOR: Fixed build failure in the decklink plugin on vs2010 due to invalid enum conversions.

--- src/openmedialib/plugins/decklink/src/decklink_utilities.cpp
+++ src/openmedialib/plugins/decklink/src/decklink_utilities.cpp
@@ -120,7 +120,8 @@ BMDDisplayMode frame_to_display_mode( const ml::frame_type_ptr& frame )
 	ARENFORCE_MSG( false, "Could not convert frame to decklink display mode" )
 		( fps_num )( fps_den )( frame->height() )( img->field_order() );
 
-	return bmdDisplayModeNotSupported;
+	//Never reached
+	return bmdModePAL;
 }
 	
 	
@@ -172,8 +173,8 @@ BMDAudioSampleType frame_to_audio_sample_type( const ml::frame_type_ptr& frame )
 	}
 	
 	ARENFORCE_MSG( false, "Unsupported audio sample width" )( aud->af() );
-
-	return 0;
+	//Never reached
+	return bmdAudioSampleType16bitInteger;
 }
 	
 } } } }
